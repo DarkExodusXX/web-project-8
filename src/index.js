d3ae7d3cc608155077e88f0c0c235dc607eaa791
Denis Goncharov <darkexodusxx@gmail.com>
2020-11-18 15:43:42 +0300

    1

--- src/index.js
+++ src/index.js
@@ -3,10 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter basename="/web-project-8">
+      <App />
+    </BrowserRouter>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
